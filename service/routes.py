3a162abd8a3954b5d64ebbacc68f1407515d059c
TJPrims <topri@danskebank.dk>
2025-10-07 14:56:09 -0400

    Fix linting issues (W292, W293)

--- service/routes.py
+++ service/routes.py
@@ -102,7 +102,7 @@ def update_account(account_id):
     account = Account.find(account_id)
     if not account:
         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-    
+
     account.deserialize(request.get_json())
     account.update()
     return jsonify(account.serialize()), status.HTTP_200_OK
@@ -139,3 +139,4 @@ def check_content_type(media_type):
         status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
         f"Content-Type must be {media_type}",
     )
+    
